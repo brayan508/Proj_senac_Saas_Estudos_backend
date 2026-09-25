983828c1d78d7758a64f2012ca7c23739004b374
brayan508 <brayanrodrigues713@gmail.com>
2026-09-25 16:09:29 -0300

    primeiro commit

---
+++ src/lib/prisma.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+
+import postgres from "@prisma/orm-postgres/runtime";
+
+// Apontando para o contract gerado, não mais para schema
+
+import type { Contract } from "../../prisma/contract.d.ts"; 
+
+import contractJson from "../../prisma/contract.json" with { type: "json" };
+
+
+export const prisma = postgres<Contract>({
+
+  contractJson,
+
+  url: process.env.DATABASE_URL!, 
+
+});
